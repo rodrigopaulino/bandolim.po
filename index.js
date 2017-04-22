bbd0bddccd812e8d300e41dc37d5e7f6a94e319d
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2017-04-22 17:47:04 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
 
-const vorpal = require('vorpal')(); //???
-const request = require('request');
-const fs = require('fs');
-const shell = require('shelljs');
+const vorpal = require("vorpal")();
+const request = require("request");
+const fs = require("fs");
+const shell = require("shelljs");
 const patchingToolPath = "./patching-tool";
 const patchesPath = `${patchingToolPath}/patches`;
-const prefixURL = "http://nikita/private/ee/fix-packs/6.2.10";
+const prefixURL = "https://nikita/private/ee/fix-packs/6.2.10";
 
 function revertAndClear() {
   shell.exec(`${patchingToolPath}/patching-tool.sh revert`);
-  shell.rm('-f', `${patchesPath}/*`);
+  shell.rm("-f", `${patchesPath}/*`);
+}
+
+function downloadAndInstall(url, fileName) {
+  request
+    .get(url)
+    .on("response", function(response) {
+      if (response.statusCode == 200) {
+        vorpal.activeCommand.log("Downloading...");
+      }
+      else if (response.statusCode == 404) {
+        vorpal.activeCommand.log("Patch not found!");
+      }
+      else {
+        vorpal.activeCommand.log(`Resquest status: ${response.statusCode}`);
+      }
+    })
+    .pipe(
+      fs.createWriteStream(`${patchesPath}/${fileName}`)
+    ).on("finish", function () {
+      shell.exec(`${patchingToolPath}/patching-tool.sh install`);
+    });
 }
 
 vorpal
@@ -20,15 +41,8 @@ vorpal
       const fileName = `liferay-fix-pack-portal-${args.level}-6210.zip`;
       const url = `${prefixURL}/portal/${fileName}`;
 
-      // revertAndClear();
-
-      request
-        .get(url)
-        .pipe(
-          fs.createWriteStream(`${patchesPath}/${fileName}`)
-        );
-
-      //shell.exec(`${patchingToolPath}/patching-tool.sh install`);
+      revertAndClear();
+      downloadAndInstall(url, fileName);
     }
   )
 
@@ -39,16 +53,9 @@ vorpal
       const fileName = `liferay-hotfix-${args.level}-6210.zip`;
       const url = `${prefixURL}/hotfix/${fileName}`;
 
-      // revertAndClear();
-
-      request
-        .get(url)
-        .pipe(
-          fs.createWriteStream(`${patchesPath}/${fileName}`)
-        );
-
-      //shell.exec(`${patchingToolPath}/patching-tool.sh install`);
+      revertAndClear();
+      downloadAndInstall(url, fileName);
     }
   )
 
-vorpal.delimiter("").show().parse(process.argv);
+vorpal.parse(process.argv);
