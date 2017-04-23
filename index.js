e7835e9b4fa974d347a16f7f53e858af2fa17003
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2017-04-23 18:37:42 -0300

    Version 1.0.0 for fixpack and hotfix commands

--- index.js
+++ index.js
@@ -4,58 +4,64 @@ const vorpal = require("vorpal")();
 const request = require("request");
 const fs = require("fs");
 const shell = require("shelljs");
+const del = require("del");
 const patchingToolPath = "./patching-tool";
 const patchesPath = `${patchingToolPath}/patches`;
 const prefixURL = "http://nikita/private/ee/fix-packs/6.2.10";
 
-function revertAndClear() {
-  shell.exec(`${patchingToolPath}/patching-tool.sh revert`);
-  shell.rm("-f", `${patchesPath}/*`);
-}
-
-function downloadAndInstall(url, fileName) {
-  request
-    .get(url)
-    .on("response", function(response) {
-      if (response.statusCode == 200) {
-        vorpal.activeCommand.log("Downloading...");
-      }
-      else if (response.statusCode == 404) {
-        vorpal.activeCommand.log("Patch not found!");
-      }
-      else {
-        vorpal.activeCommand.log(`Resquest status: ${response.statusCode}`);
-      }
-    })
-    .pipe(
-      fs.createWriteStream(`${patchesPath}/${fileName}`)
-    ).on("finish", function () {
-      shell.exec(`${patchingToolPath}/patching-tool.sh install`);
-    });
+function downloadAndInstall(url, destFile) {
+	var statusCode;
+	request
+		.get(url)
+		.on("response", function(response) {
+			statusCode = response.statusCode;
+			if (statusCode == 200) {
+				vorpal.activeCommand.log("Downloading patch...");
+			}
+			else if (statusCode == 404) {
+				vorpal.activeCommand.log("Patch not found!");
+			}
+			else {
+				vorpal.activeCommand.log(`Resquest status: ${response.statusCode}`);
+			}
+		})
+		.pipe(
+			fs.createWriteStream(destFile)
+		).on("finish", function () {
+			if (statusCode == 200) {
+				del([`${patchesPath}/*`, `!${destFile}`])
+				.then(paths => {
+					shell.exec(`${patchingToolPath}/patching-tool.sh install -force`);
+				});
+			}
+			else {
+				del(destFile);
+			}
+		});
 }
 
 vorpal
-  .command("fixpack <level>")
-  .action(
-    (args, callback) => {
-      const fileName = `liferay-fix-pack-portal-${args.level}-6210.zip`;
-      const url = `${prefixURL}/portal/${fileName}`;
+	.command("fixpack <level>")
+	.action(
+		(args, callback) => {
+			var fileName = `liferay-fix-pack-portal-${args.level}-6210.zip`;
+			var url = `${prefixURL}/portal/${fileName}`;
+			var destFile = `${patchesPath}/${fileName}`;
 
-      revertAndClear();
-      downloadAndInstall(url, fileName);
-    }
-  )
+			downloadAndInstall(url, destFile);
+		}
+	)
 
 vorpal
-  .command("hotfix <level>")
-  .action(
-    (args, callback) => {
-      const fileName = `liferay-hotfix-${args.level}-6210.zip`;
-      const url = `${prefixURL}/hotfix/${fileName}`;
+	.command("hotfix <level>")
+	.action(
+		(args, callback) => {
+			var fileName = `liferay-hotfix-${args.level}-6210.zip`;
+			var url = `${prefixURL}/hotfix/${fileName}`;
+			var destFile = `${patchesPath}/${fileName}`;
 
-      revertAndClear();
-      downloadAndInstall(url, fileName);
-    }
-  )
+			downloadAndInstall(url, destFile);
+		}
+	)
 
 vorpal.parse(process.argv);
