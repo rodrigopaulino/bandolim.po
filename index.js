6c5f6de080edd4e080aeaed71b5aec30a0b2da58
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2017-05-12 15:19:07 -0300

    Search for difference was being limited by the max result default property

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 #!/usr/bin/env node
 
-const vorpal = require("vorpal")();
 // const request = require("request");
 // const fs = require("fs");
 // const shell = require("shelljs");
 // const del = require("del");
-const colors = require("colors");
-const inquirer = require("inquirer");
-const JiraApi = require('jira').JiraApi;
 // const patchingToolPath = "./patching-tool";
 // const patchesPath = `${patchingToolPath}/patches`;
 // const prefixURL = "http://nikita/private/ee/fix-packs/6.2.10";
-const questions = [
-	{
-		type: "input",
-		message: colors.magenta("JIRA's username:"),
-		name: "username",
-		required: true
-	},
-	{
-		type: "password",
-		message: colors.magenta("JIRA's password:"),
-		name: "password"
-	}
-];
-
-function log(message) {
-	vorpal.activeCommand.log(message);
-}
 
 // function downloadAndInstall(url, destFile) {
 // 	var statusCode;
@@ -61,8 +40,70 @@ function log(message) {
 // 		});
 // }
 
-function getIssues(newer, older, version, total, component, username, password) {
-	var filter = "";
+// vorpal
+// 	.command("fixpack <level>")
+// 	.action(
+// 		(args, callback) => {
+// 			var fileName = `liferay-fix-pack-portal-${args.level}-6210.zip`;
+// 			var url = `${prefixURL}/portal/${fileName}`;
+// 			var destFile = `${patchesPath}/${fileName}`;
+//
+// 			downloadAndInstall(url, destFile);
+// 		}
+// 	);
+//
+// vorpal
+// 	.command("hotfix <level>")
+// 	.action(
+// 		(args, callback) => {
+// 			var fileName = `liferay-hotfix-${args.level}-6210.zip`;
+// 			var url = `${prefixURL}/hotfix/${fileName}`;
+// 			var destFile = `${patchesPath}/${fileName}`;
+//
+// 			downloadAndInstall(url, destFile);
+// 		}
+// 	);
+const vorpal = require("vorpal")();
+const colors = require("colors");
+const inquirer = require("inquirer");
+const JiraApi = require("jira").JiraApi;
+const Promise = require("promise");
+const questions = [
+	{
+		type: "input",
+		message: colors.magenta("JIRA's username:"),
+		name: "username",
+		required: true
+	},
+	{
+		type: "password",
+		message: colors.magenta("JIRA's password:"),
+		name: "password"
+	}
+];
+const result = {
+	total: null,
+	issues: []
+};
+const options = {
+	startAt: 0,
+	maxResults: 5000,
+	fields: [
+		"id",
+		"key"
+	]
+};
+
+var tuple = "";
+var filter = "";
+
+function log(message) {
+	vorpal.activeCommand.log(message);
+}
+
+function getIssues(
+	newer, older, version, total, component, username, password) {
+
 	var label = "";
 	var tuple = "";
 	var comma = "";
@@ -89,68 +130,40 @@ function getIssues(newer, older, version, total, component, username, password) 
 
 	var jira = new JiraApi(
 		"https", "issues.liferay.com", null, username, password, 2);
-	var options = {
-		startAt: 0,
-		maxResults: 5000,
-		fields: [
-			"id",
-			"key"
-		]
-	};
 
-	function callback(error, body) {
-		if (!error) {
-			if (total) {
-				log(`Number of tickets: ${body.total}`);
-			} else {
-				var tickets = "";
-				var comma = "";
+	return new Promise((resolve, reject) => {
+			jira.searchJira(
+				`labels IN (${tuple}) ${filter} ORDER BY key ASC`,
+				options, function callback(error, body) {
+					if (error) {
+						return reject(error);
+					}
 
-				body.issues.forEach((item, index, arr) => {
-					tickets = tickets + comma + item.key;
-					comma = ", ";
+					if (!result.total) {
+						result.total = body.total;
+					}
+
+					body.issues.forEach((item, index, arr) => {
+						result.issues.push(item);
+					});
+
+					if (result.issues.length < result.total) {
+						options.startAt += body.issues.length;
+
+						return jira.searchJira(
+							`labels IN (${tuple}) ${filter} ORDER BY key ASC`,
+							options, callback);
+					} else {
+						return resolve(result);
+					}
 				});
-
-				log(tickets);
-			}
-		} else {
-			log(error);
-		}
-	}
-
-	jira.searchJira(
-		`labels IN (${tuple}) ${filter} ORDER BY key ASC`,
-		options, callback);
+		});
 }
-
-// vorpal
-// 	.command("fixpack <level>")
-// 	.action(
-// 		(args, callback) => {
-// 			var fileName = `liferay-fix-pack-portal-${args.level}-6210.zip`;
-// 			var url = `${prefixURL}/portal/${fileName}`;
-// 			var destFile = `${patchesPath}/${fileName}`;
-//
-// 			downloadAndInstall(url, destFile);
-// 		}
-// 	);
-//
-// vorpal
-// 	.command("hotfix <level>")
-// 	.action(
-// 		(args, callback) => {
-// 			var fileName = `liferay-hotfix-${args.level}-6210.zip`;
-// 			var url = `${prefixURL}/hotfix/${fileName}`;
-// 			var destFile = `${patchesPath}/${fileName}`;
-//
-// 			downloadAndInstall(url, destFile);
-// 		}
-// 	);
 
 vorpal
 	.command("diff <fixpack1> <fixpack2> <version>")
-	.option("-t, --total, Number of tickets between two patch levels.")
-	.option("-c, --component <name>, Filter by Component.")
+	.option("-t, --total", "Number of tickets between two patch levels.")
+	.option("-c, --component <name>", "Filter by Component.")
 	// .option("-g, --group', 'Group the result by Components.")
 	.action(
 		(args, callback) => {
@@ -177,7 +190,25 @@ vorpal
 			inquirer.prompt(questions).then(function (answers) {
 				getIssues(
 					newer, older, args.version, args.options.total,
-					args.options.component, answers.username, answers.password);
+					args.options.component, answers.username, answers.password)
+					.then(body => {
+						if (args.options.total) {
+							log(`Number of tickets: ${body.total}`);
+						} else {
+							var tickets = "";
+							var comma = "";
+
+							body.issues.forEach((item, index, arr) => {
+								tickets = tickets + comma + item.key;
+								comma = ", ";
+							});
+
+							log(tickets);
+						}
+					})
+					.catch(error => {
+						log(error);
+					});
 			});
 		}
 	);
